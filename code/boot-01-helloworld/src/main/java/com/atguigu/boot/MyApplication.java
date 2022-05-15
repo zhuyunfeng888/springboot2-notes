7d8e406ae4afef296d6aef9aaad47e87992798ad
zhuyunfeng <2495849805@qq.com>
2022-05-15 11:46:48 +0800

    自动配置特性

--- code/boot-01-helloworld/src/main/java/com/atguigu/boot/MyApplication.java
+++ code/boot-01-helloworld/src/main/java/com/atguigu/boot/MyApplication.java
@@ -1,18 +1,35 @@
 package com.atguigu.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 主程序类
  *
  * @SpringBootApplication: 这是一个 SpringBoot 应用
  */
-@SpringBootApplication
+// @SpringBootApplication(scanBasePackages = "com.atguigu")
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan("com.atguigu.boot")
 public class MyApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(MyApplication.class, args);
+        // 1. 返回 IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MyApplication.class, args);
+
+        // 2. 查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names)
+        {
+            System.out.println(name);
+        }
+
+
     }
 }
